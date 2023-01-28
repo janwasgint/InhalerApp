fb8a7667a7b72a64bf64dda87bea3c4df9aef1a0
Jan Wasgint <jan.wasgint@motius.de>
2023-01-28 14:22:03 +0100

    Play around with UI elements

--- inhaler_app/lib/detail_page.dart
+++ inhaler_app/lib/detail_page.dart
@@ -8,6 +8,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  bool isSwitchActive = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +23,63 @@ class _DetailPageState extends State<DetailPage> {
         ),
       ),
       body: Column(
-        children: [Image.asset('images/inhaler.jpg')],
+        children: [
+          Image.asset(
+            'images/inhaler.jpg',
+          ),
+          const SizedBox(height: 10),
+          const Divider(color: Colors.black),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text('This is how you inhale',
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint('Elevated button');
+            },
+            child: const Text('Button'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint('Outlined button');
+            },
+            child: const Text('Button'),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint('Text button');
+            },
+            child: const Text('Button'),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint('This is the row');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Icon(Icons.local_fire_department, color: Colors.blue),
+                Text('Row widget'),
+                Icon(Icons.local_fire_department, color: Colors.blue),
+              ],
+            ),
+          ),
+          Switch(
+            value: isSwitchActive,
+            onChanged: (bool newValue) {
+              setState(() {
+                isSwitchActive = newValue;
+              });
+            },
+          ),
+        ],
       ),
     );
   }
